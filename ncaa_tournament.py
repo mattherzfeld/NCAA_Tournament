1609160fcc648096555681a9f96138bb51d6ac9c
Matt <54761045+mattherzfeld@users.noreply.github.com>
2025-04-19 23:52:08 -0500

    Update ncaa_tournament.py

--- ncaa_tournament.py
+++ ncaa_tournament.py
@@ -4,6 +4,15 @@ import random
 
 team1 = "Auburn"
 team2 = "Wisconsin"
+team3 = "Marquette"
+team4 = "Texas"
+
+teams = {
+    team1: "Tigers",
+    team2: "Badgers",
+    team3: "Golden Eagles",
+    team4: "Longhorn",
+}
 
 def randomizer():
     x1 = random.randint(0,10)
